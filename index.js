7600e9f38a822d72079b621e20ff7700c053a608
Karthikeyan Govindaraj <karthikeyan.govindaraj@blackrock.com>
2018-03-30 01:25:11 -0700

    isEmptyObj

---
+++ index.js
@@ -0,0 +1,64 @@
+/**
+ * Checks the given object (recursively if needed) and
+ * returns the object with its associated binary value for
+ * isEmpty evaluation
+ * @param obj
+ * @returns {*}
+ */
+function nestedEmptyCheck(obj) {
+
+    if(obj === "null") {
+        return true;
+    }
+
+    else if(typeof obj === "number"){
+        return false;
+    }
+
+    else if((typeof obj === "string") && obj != ''){
+        return false;
+    }
+
+    else if(Array.isArray(obj)){
+        if(obj.length === 0) {
+            return true;
+        } else {
+            for(var i = 0; i < obj.length; i++) {
+                obj[i] = nestedEmptyCheck(obj[i]);
+            }
+            return obj;
+        }
+    }
+
+    else if(typeof obj === "object"){
+        var primaryKeys = Object.keys(obj);
+        if(primaryKeys && primaryKeys.length === 0) {
+            return true;
+        } else {
+            primaryKeys.forEach(key => {
+                var value = nestedEmptyCheck(obj[key]);
+            obj[key] = value;
+        });
+            return obj;
+        }
+    }
+    return obj;
+}
+
+/**
+ * before calling the nested validation method,
+ * makes a copy of the original object and calls with the copy of the object.
+ * @param object
+ * @returns {boolean}
+ */
+function isEmptyObj(object) {
+    var objToSend = JSON.parse(JSON.stringify(object));
+    var result = nestedEmptyCheck(objToSend);
+    if(JSON.stringify(result).indexOf('false') > -1)
+        return false;
+    return true;
+}
+
+
+
+module.exports = isEmptyObj
---
+++ test/index.js
@@ -0,0 +1,32 @@
+var test = require('tape')
+var isEmpty = require('../index')
+
+
+var a = [];
+var b = [{1:2}];
+var c = { one: 1 };
+var d = {};
+var e = [{}];
+var f = {source: {}, type: 'typeValue', pool: {}, acquireConnectionTimeout: 30000, system: [ { a: [1,2], c: [5,6] }, 2] }
+var g = {timeseries: {tillDate: ['val1', 'val2', 'val3', 'val4', 'val5']}}
+var h = { timeseries: { tillDate: [], } }
+var i = {a: [], b:{ }};
+var j = {a: [], b:{ aa:[11], bb:{}}};
+var k ={ system: [ { a: [1,2], c: [5,6], }, 2]}
+var l = {source: {}, type: "", pool: {}, acquireConnectionTimeout: '', system: [ { a: [], c: [] }, ] }
+
+test('isEmptyObj Testing', (assert) => {
+    assert.equal(isEmpty(a), true)
+    assert.equal(isEmpty(b), false)
+    assert.equal(isEmpty(c), false)
+    assert.equal(isEmpty(d), true)
+    assert.equal(isEmpty(e), true)
+    assert.equal(isEmpty(f), false)
+    assert.equal(isEmpty(g), false)
+    assert.equal(isEmpty(h), true)
+    assert.equal(isEmpty(i), true)
+    assert.equal(isEmpty(j), false)
+    assert.equal(isEmpty(k), false)
+    assert.equal(isEmpty(l), true)
+    assert.end()
+})
